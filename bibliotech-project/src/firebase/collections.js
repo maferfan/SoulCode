5e0e630339aa89b0fcaeed1d67e6a0a5330fd1f9
maferfan <matheusferrazza@gmail.com>
2023-06-22 12:47:02 -0300

    att

--- bibliotech-project/src/firebase/collections.js
+++
@@ -1,5 +0,0 @@
-import { db } from "./config";
-import { collection } from "firebase/firestore";
-
-export const livrosCollection = collection(db, "livros");
-export const emprestimosCollection = collection(db, "emprestimos");
--- bibliotech-project/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import "./index.css";
-import "./pages/Home/Home"
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<App />);
